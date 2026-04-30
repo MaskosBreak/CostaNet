00260c765ed4fe28b444e0a9b25b2982c73ba4fc
Masko <maskosbreak@gmail.com>
2026-04-30 09:30:17 -0300

    Add files via upload

--- calculos/script.js
+++ calculos/script.js
@@ -5,3 +5,15 @@ function multiplicar(){
     res.textContent = n1 * n2
     console.log(n1,n2)
 }
+function dividir(){
+    var n3 = parseFloat(document.getElementById("n3").value)
+    var n4 = parseFloat(document.getElementById("n4").value)
+    var res = document.getElementById("resp")
+    if(n4!==0){
+    resp.textContent = n3 / n4
+    }
+    else{
+        resp.textContent = "Ce é burro pac?"
+    }
+    console.log(n3,n4)
+}
